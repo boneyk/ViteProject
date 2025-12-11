1738e2607fa95e65adf5a4e6b96d955ed368eabf
boneyk <124228691+boneyk@users.noreply.github.com>
2025-12-11 16:44:32 +1000

    MUI was added to the project.
    Card was remade using MUI components

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,32 +1,34 @@
-import { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { taskStore } from "../store/TaskStore";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 const ToDo = observer(() => {
   return (
     <>
-      {taskStore.tasks.map((task, ind) => {
-        return (
-          <div
-            key={ind}
-            onClick={() => taskStore.toggleTask(ind)}
-            style={{
-              width: "400px",
-              height: "120px",
-              border: "2px solid black",
-              background: "grey",
-              cursor: "pointer",
-              margin: "10px"
-            }}
-          >
-            <h3 style={{ textDecoration: task.doFlag ? "line-through" : "none" }}>
+      {taskStore.tasks.map((task, ind) => (
+        <Card
+          sx={{
+            maxWidth: 345,
+            border: "2px solid black",
+            cursor: "pointer",
+            margin: "10px"
+          }}
+          key={ind}
+          onClick={() => taskStore.toggleTask(ind)}
+        >
+          <CardContent>
+            <Typography
+              variant="h6"
+              sx={{ textDecoration: task.doFlag ? "line-through" : "none" }}
+            >
               {task.title}
-            </h3>
-            <p>
-              {task.desc}
-            </p>
-          </div>);
-      })}
+            </Typography>
+            <Typography variant="body2">{task.desc}</Typography>
+          </CardContent>
+        </Card>
+      ))}
     </>
   );
 });
